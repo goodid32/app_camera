e13113d4679dbb57010c938c97958452ebbbddc6
DICCE <goodid32@gmail.com>
2023-01-17 15:02:20 +0900

    Initial commit

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import os
+
+img_path = os.path.join(os.path.dirname(__file__), 'images')
+st.subheader('학생 사진 촬영')
+
+col1, col2 = st.columns(2)
+
+hakbun = col1.text_input('학번')
+name = col2.text_input('성명')
+
+pic = st.camera_input('사진찍기')
+
+if pic is not None:
+    img_name = hakbun+name
+    frame, ext = os.path.splitext(pic.name)
+    with open(os.path.join(img_path, img_name+ext), 'wb') as f:
+        f.write(pic.getbuffer())
+
